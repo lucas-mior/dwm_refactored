db8fe5d7563853305c32bc29acb981de84dcc436
lucas-mior <lucas.mior@tutamail.com>
2023-11-04 20:16:30 -0300

    config: colors configuration

--- config.def.h
+++ config.def.h
@@ -14,22 +14,18 @@ static const char *fonts[]          = {
 	"Noto Color Emoji:size=11:antialias=true:autohint=true",
 	"LiterationMono Nerd Font:size=11:style=Regular",
 };
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeUrg]  = { col_gray4, col_cyan,  "red"  },
+	/*               fg         bg         border  */
+	[SchemeNorm] = { "#ffffff", "#000000",   "#000000" },
+	[SchemeInv]  = { "#000000", "#004400", "#000000" },
+	[SchemeSel]  = { "#ffffff", "#004400", "green"   },
+	[SchemeUrg]  = { "red",     "red",     "red"     },
 };
 static const unsigned int alphas[][3] = {
 	/*               fg      bg    border */
-	[SchemeNorm] = { OPAQUE, 0xbb, OPAQUE },
+	[SchemeNorm] = { OPAQUE, 0xbb, 0x00   },
 	[SchemeSel]  = { OPAQUE, 0xbb, OPAQUE },
-	[SchemeUrg]  = { OPAQUE, 0xbb, OPAQUE  },
+	[SchemeUrg]  = { OPAQUE, 0xbb, OPAQUE },
 };
 
 /* tagging */
