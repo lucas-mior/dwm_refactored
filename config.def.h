c660d9760a26e23cc526c58a4772e4ef96cd5e17
lucas-mior <lucas.mior@tutamail.com>
2023-11-08 10:39:55 -0300

    add superKey

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int tabModKey = 0x40;
 static const unsigned int tabCycleKey = 0x17;
+static const unsigned int superKey = 133;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
