57ea914635a02cd65d9492f54a2b0428b654d5c2
lucas-mior <lucas.mior@tutamail.com>
2023-11-08 00:17:17 -0300

    patch: column

--- config.def.h
+++ config.def.h
@@ -55,6 +55,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "###",      gaplessgrid },
+	{ "|||",      col },
 };
 
 /* key definitions */
