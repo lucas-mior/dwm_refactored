696738c75d3c21a73cf243d9c807935af76a9b4e
lucas-mior <lucas.mior@tutamail.com>
2024-08-26 14:34:45 -0300

    refactor

--- dwm.c
+++ dwm.c
@@ -1453,7 +1453,7 @@ Picture
 get_icon_property(Window window, uint *picture_width, uint *picture_height) {
     int format;
     ulong nitems_return;
-    ulong extra;
+    ulong bytes_after_return;
     ulong *prop_return = NULL;
     ulong *pixel_find = NULL;
     uint32 *pixel_find32;
@@ -1468,7 +1468,7 @@ get_icon_property(Window window, uint *picture_width, uint *picture_height) {
     if (XGetWindowProperty(display, window, netatom[NetWMIcon],
                            0L, LONG_MAX, False, AnyPropertyType,
                            &real, &format,
-                           &nitems_return, &extra, (uchar **)&prop_return) != Success) {
+                           &nitems_return, &bytes_after_return, (uchar **)&prop_return) != Success) {
         return None;
     }
     if (nitems_return == 0 || format != 32) {
