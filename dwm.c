daa4009cdd37ae3d528637c2e47059c87db096a2
lucas-mior <lucas.mior@tutamail.com>
2024-08-29 19:16:26 -0300

    style

--- dwm.c
+++ dwm.c
@@ -2533,9 +2533,8 @@ monitor_draw_bar(Monitor *monitor) {
     /* bottom bar */
     drw_setscheme(drw, scheme[SchemeNormal]);
     drw_rect(drw, 0, 0, (uint)monitor->win_w, bar_height, 1, 1);
-    if (monitor == live_monitor) {
+    if (monitor == live_monitor)
         draw_status_text(&status_bottom, monitor->win_w);
-    }
     drw_map(drw, monitor->bottom_bar_window,
             0, 0, (uint)monitor->win_w, bar_height);
     return;
